0da445c25e9dce6138ae317840256500c9f8de6b
Arvind <arvindvyas07@gail.com>
2012-12-17 19:50:59 +0530

    model for login with identy

---
+++ app/models/identity.rb
@@ -0,0 +1,13 @@
+
+
+
+class Identity < OmniAuth::Identity::Models::ActiveRecord
+ has_secure_password
+ before_save { |identity| identity.email = email.downcase }
+
+  attr_accessible :email, :name, :user_type, :password_digest, :password, :password_confirmation
+
+  validates_uniqueness_of :email
+  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+end
+
