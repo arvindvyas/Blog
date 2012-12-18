75874c395c53b7d20816d73d84f73b3e2ab513ad
Arvind <arvindvyas07@gail.com>
2012-12-18 16:48:03 +0530

    add association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,15 @@
 class Post < ActiveRecord::Base
- has_attached_file :photo, :styles => { :small => "150x150>" },
+  has_attached_file :photo, :styles => { :small => "150x150>" },
                   :url  => "/assets/post/:id/:style/:basename.:extension",
                   :path => ":rails_root/public/assets/post/:id/:style/:basename.:extension"
 
-validates_attachment_presence :photo
-validates_attachment_size :photo, :less_than => 5.megabytes
-validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
+  
+    has_many :comments, :dependent => :destroy
+    accepts_nested_attributes_for :comments, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
+  
 
-
-
-  attr_accessible :article, :comments, :image, :title
+  attr_accessible :article, :comments, :photo, :title
 end
