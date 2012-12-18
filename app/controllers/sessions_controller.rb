b4f12217ca17f9d6aa42bdb084de0f87c69c62ce
Arvind <arvindvyas07@gail.com>
2012-12-18 11:57:15 +0530

    redirect to the post controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = @user.id
-    redirect_to root_url, notice: "Signed in!"
+    redirect_to post_path, notice: "Signed in!"
   end
 
   def destroy
