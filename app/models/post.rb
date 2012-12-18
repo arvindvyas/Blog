11a853f590b9e8be1d66fcce45118ccd6562e713
Arvind <arvindvyas07@gail.com>
2012-12-18 16:08:08 +0530

    add paperclip validation adn configuration"

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ActiveRecord::Base
+ has_attached_file :photo, :styles => { :small => "150x150>" },
+                  :url  => "/assets/post/:id/:style/:basename.:extension",
+                  :path => ":rails_root/public/assets/post/:id/:style/:basename.:extension"
+
+validates_attachment_presence :photo
+validates_attachment_size :photo, :less_than => 5.megabytes
+validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
+
+
+
   attr_accessible :article, :comments, :image, :title
 end
